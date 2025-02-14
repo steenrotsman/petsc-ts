fbd6579d56f4760f0851cf84af0caf674d99d91c
Stijn <s.rotman@tutanota.com>
2025-02-14 11:36:39 +0100

    Correct use of partial.

--- examples/ucr.py
+++ examples/ucr.py
@@ -11,8 +11,8 @@ PATH = "data/UCRArchive_2018"
 
 load_ucr = partial(load_UCR_UEA_dataset, return_type="np3d", extract_path=PATH)
 for name in NAMES:
-    X_train, y_train = load_UCR_UEA_dataset(name, split="train")
-    X_test, y_test = load_UCR_UEA_dataset(name, split="test")
+    X_train, y_train = load_ucr(name, split="train")
+    X_test, y_test = load_ucr(name, split="test")
     pets = PetsClassifier(duration=1, multiresolution=True, soft=True)
     pets.fit(X_train, y_train)
     y_pred = pets.predict(X_test)
