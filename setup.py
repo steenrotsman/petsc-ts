d1d4c46977ca5548df4b3d81057ecad4c42273e1
stijn <s.j.rotman@tilburguniversity.edu>
2025-03-14 13:08:14 +0100

    Change name of C++ module from petsc_miner to _petsc_miner.

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ __version__ = "0.1"
 
 ext_modules = [
     Pybind11Extension(
-        "petsc_miner",
+        "_petsc_miner",
         ["src/bind.cpp", "src/miner.cpp", "src/pattern.cpp"],
         define_macros=[("VERSION_INFO", __version__)],
     ),
 ]
 
 setup(
-    name="petsc_miner",
+    name="_petsc_miner",
     ext_modules=ext_modules,
     cmdclass={"build_ext": build_ext},
 )
